34ab76f8ec5e71bd0c7ce11a491ebfc20438653f
Max Henry <mhenry@eers.ca>
2023-05-13 17:28:58 -0400

    First commit with test import from package

---
+++ iverb/test.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def adder(a, b):
+    return a + b
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="infinite-reverb",
+    version="0.1.0",
+    packages=find_packages(),
+)
