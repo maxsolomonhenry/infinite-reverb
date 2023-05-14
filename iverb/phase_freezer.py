87d139c0f4187aa6da0dd98ffd4fd585688a5f2e
Max Henry <mhenry@eers.ca>
2023-05-14 18:07:26 -0400

    Add main vocoder functionality

--- iverb/phase_freezer.py
+++ iverb/phase_freezer.py
@@ -6,17 +6,54 @@ class PhaseFreezer(OlaBuffer):
     def __init__(self, frame_size, num_overlap):
         super().__init__(frame_size, num_overlap)
         self._window = np.hamming(frame_size)
-        self._do_frame_drop = False
+
+        self._do_freeze = False
+        self._do_grab_frame_one = False
+        self._do_grab_frame_two = False
+
+        hN = frame_size // 2 + 1
+        self._fft_buffer = np.zeros([hN, 2], dtype=complex)
+
+        self._magnitude = np.zeros(hN)
+        self._delta_phase = np.zeros(hN)
+        self._phase = np.zeros(hN)
+
+    def request_freeze(self):
+        self._do_grab_frame_one = True
+
+    def request_unfreeze(self):
+        self._do_freeze = False
 
     def _processor(self, frame):
 
-        tmp = frame.copy()
+        frame *= self._window
 
-        if self._do_frame_drop:
-            tmp *= 0
-            self._do_frame_drop = False
+        if self._do_grab_frame_one:
+            self._fft_buffer[:, 0] = np.fft.rfft(frame)
+            self._do_grab_frame_one = False
+            self._do_grab_frame_two = True
 
-        return tmp * self._window
+        if self._do_grab_frame_two:
+            self._fft_buffer[:, 1] = np.fft.rfft(frame)
+            self._do_grab_frame_two = False
+
+            self._init_freeze()
+            self._do_freeze = True
+
+        if self._do_freeze:
+            self._phase += self._delta_phase
+            frame = self._magnitude * np.exp(1j * self._phase)
+            frame = np.fft.irfft(frame)
+            frame *= self._window
+
+        return frame
     
-    def request_drop(self):
-        self._do_frame_drop = True
+    def _init_freeze(self):
+        self._magnitude = np.abs(self._fft_buffer[:, 1])
+
+        one = np.angle(self._fft_buffer[:, 0])
+        two = np.angle(self._fft_buffer[:, 1])
+        self._delta_phase = (two - one) % (2 * np.pi)
+
+        self._phase = two
+    
